fea2ceb147733c17aeef188551cafe0028aba6f5
张龙 <zhanglong@weipaitang.com>
2022-05-20 22:36:34 +0800

    feat: 合成事件

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
-import React from "./react";
-import ReactDom from "./react-dom";
+import React from "react";
+import ReactDom from "react-dom";
+
+/**
+ * 合成事件和批量更新
+ * 1. 在react里面，事件的更新可能是异步的，是批量的，不是同步的
+ *  调用setState之后状态并没有立刻更新，而是先缓存起来，
+ *  等事件函数处理完成之后，再进行批量更新，一次更新并重新渲染。
+ *  原因：如果我们一个函数触发多个setState，第一次set1 第二次set2 第三次又set1，这样就会造成没必要的render
+ *  因为只要调用setState就会进行render
+ *
+ *
+ */
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {num: 1, name: 'zl'}
+    this.state = {num: 0, name: 'zl'}
   }
 
   handleClick = () => {
     this.setState({num: this.state.num + 1});
+    console.log(this.state.num);
+    this.setState({num: this.state.num + 1});
+    console.log(this.state.num);
+    setTimeout(() => {
+      this.setState({num: this.state.num + 1});
+      console.log(this.state.num);
+      this.setState({num: this.state.num + 1});
+      console.log(this.state.num);
+    },100)
   }
   render() {
     return (
